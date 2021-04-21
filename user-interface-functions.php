81d573d32520deed081a53a1222a477442be67a3
Zack Jones <zackaryj@rams.colostate.edu>
2021-04-21 14:54:20 -0600

    Split up interface buttons function

--- user-interface-functions.php
+++ user-interface-functions.php
@@ -5,6 +5,20 @@ function kcw_logs_build_main_list_item_button_data($name, $action) {
     return array("name"=>"$name","action"=>"$action");
 }
 
+
+function kcw_logs_add_basic_buttons($buttons) {
+    $buttons[] = kcw_logs_interface_append_button("my-logs", "//");
+    $buttons[] = kcw_logs_interface_append_button("projects", "//");
+    $buttons[] = kcw_logs_interface_append_button("customers", "//"); 
+    return $buttons;
+}
+
+function kcw_logs_add_manage_buttons($buttons) {
+    $buttons[] = kcw_logs_interface_append_button("shop-logs", "//");
+    $buttons[] = kcw_logs_interface_append_button("project-reports", "//"); 
+    return $buttons;
+}
+
 function kcw_logs_determine_interface_buttons() {
     $buttons = array();
 
@@ -14,19 +28,11 @@ function kcw_logs_determine_interface_buttons() {
     };
 
     if (kcw_logs_wp_current_is_priviledged()) {
-        if (kcw_logs_current_can_manage_own()) {
-            $buttons[] = kcw_logs_interface_append_button("my-logs", "//");
-            $buttons[] = kcw_logs_interface_append_button("projects", "//");
-            $buttons[] = kcw_logs_interface_append_button("customers", "//"); 
-        }
-        if (kcw_logs_current_can_manage_shop()) {
-            $buttons[] = kcw_logs_interface_append_button("shop-logs", "//");
-            $buttons[] = kcw_logs_interface_append_button("project-reports", "//");
-        }
-        if (kcw_logs_current_can_manage_all()) {
-            $buttons[] = kcw_logs_interface_append_button("manage-staff", "//");
-        }
+        if (kcw_logs_current_can_manage_own()) $buttons = kcw_logs_add_basic_buttons($buttons);
+        if (kcw_logs_current_can_manage_shop()) $buttons = kcw_logs_add_manage_buttons($buttons);
+        if (kcw_logs_current_can_manage_all()) $buttons[] = kcw_logs_interface_append_button("manage-staff", "//");
     }
+    
     return $buttons;
 }
 ?>
