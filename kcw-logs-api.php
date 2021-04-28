494362ba396004980c90ed99652a757123987ee7
Zack Jones <zackaryj@rams.colostate.edu>
2021-04-28 12:43:30 -0600

    Add search routes & methods

--- kcw-logs-api.php
+++ kcw-logs-api.php
@@ -5,25 +5,40 @@ include_once "kcw-logs-api-helpers.php";
 
 $kcw_logs_api_url = home_url('wp-json/' . $kcw_logs_api_namespace . '/v1/');
 
-//Return a list of timesheets
+//Return a list of timesheets owned by the given user
 function kcw_logs_api_list_Timesheets($data) {
     $allowed = kcw_logs_api_validate_request_data($data, "list-timesheets");
     return $allowed;
 }
+function kcw_logs_api_search_Timesheets($data) {
+    $allowed = kcw_logs_api_validate_request_data($data, "search-timesheets");
+    return $data["search"];
+}
+
 //Return a list of projects
 function kcw_logs_api_list_Projects($data) {
     $allowed = kcw_logs_api_validate_request_data($data, "list-projects");
     return $allowed;
 }
+function kcw_logs_api_search_Projects($data) {
+    $allowed = kcw_logs_api_validate_request_data($data, "search-projects");
+    return $data["search"];
+}
+
 //Return a list of staff
 function kcw_logs_api_list_Staff($data) {
     $allowed = kcw_logs_api_validate_request_data($data, "list-staff");
     return $allowed;
 }
+
 //Return a list of customers
 function kcw_logs_api_list_Customers($data) {
     $allowed = kcw_logs_api_validate_request_data($data, "list-customers");
     return $allowed;
+}
+function kcw_logs_api_search_Customers($data) {
+    $allowed = kcw_logs_api_validate_request_data($data, "search-customers");
+    return $data["search"];
 }
 
 ?>
--- kcw-logs-rest-routes.php
+++ kcw-logs-rest-routes.php
@@ -1,12 +1,20 @@
 <?php
 
 $kcw_logs_api_namespace = "kcwlogs";
+//Allow WP Token format (alphanumber)
 $kcw_logs_auth_token_rest_format = "(?P<token>[a-zA-Z0-9]+)";
+//Allow only numbers
+$kcw_logs_limit_rest_format = "(?P<limit>[0-9]?)";
+//Allow alphanumberic and a DOT or DASH
+$kcw_logs_search_rest_format = "(?P<search>[a-zA-Z0-9-\.]+)";
+
 
 function kcw_logs_api_RegisterTimesheetRoutes() {
     global $kcw_logs_api_namespace;
     global $kcw_logs_auth_token_rest_format;
+    global $kcw_logs_search_rest_format;
     $token = $kcw_logs_auth_token_rest_format;
+    $search_str = $kcw_logs_search_rest_format;
     
     //Route for listing the current users timesheets
     //Returns auth error if the token cant view timesheets
@@ -15,9 +23,23 @@ function kcw_logs_api_RegisterTimesheetRoutes() {
         'callback' => 'kcw_logs_api_list_Timesheets',
     ));
 
+    //Route for search all timesheets owned by the current user
+    //Retuns any timesheets visible to the current user, including their own
+    register_rest_route( "$kcw_logs_api_namespace/v1", "/$token/timesheets/search/$search_str", array(
+        'methods' => 'GET',
+        'callback' => 'kcw_logs_api_search_Timesheets',
+    ));
+
     //Route for listing all timesheets visible to the given user
     //Retuns any timesheets visible to the current user, including their own
     register_rest_route( "$kcw_logs_api_namespace/v1", "/$token/timesheets/all", array(
+        'methods' => 'GET',
+        'callback' => 'kcw_logs_',
+    ));
+
+    //Route for search through all timesheets visible to the given user
+    //Retuns any timesheets visible to the current user, including their own
+    register_rest_route( "$kcw_logs_api_namespace/v1", "/$token/timesheets/all/search", array(
         'methods' => 'GET',
         'callback' => 'kcw_logs_',
     ));
@@ -40,7 +62,9 @@ function kcw_logs_api_RegisterTimesheetRoutes() {
 function kcw_logs_api_RegisterProjectRoutes() {
     global $kcw_logs_api_namespace;
     global $kcw_logs_auth_token_rest_format;
+    global $kcw_logs_search_rest_format;
     $token = $kcw_logs_auth_token_rest_format;
+    $search_str = $kcw_logs_search_rest_format;
     
     //Route for listing all projects
     //If the given token isnt authenticated to view projects, returns auth error
@@ -51,9 +75,9 @@ function kcw_logs_api_RegisterProjectRoutes() {
 
     //Route for searching through all the customers in the customers table (limited based on to_show)
     //If the given token isnt authenticated to view customers, returns auth error
-    register_rest_route( "$kcw_logs_api_namespace/v1", "/$token/projects/search/(?P<projectssearch>[a-zA-Z0-9]+)/(?P<to_show>[0-9])", array(
+    register_rest_route( "$kcw_logs_api_namespace/v1", "/$token/projects/search/$search_str", array(
         'methods' => 'GET',
-        'callback' => 'kcw_logs_',
+        'callback' => 'kcw_logs_api_search_Projects',
     ));
 
     //Route for getting a project based on the given project id
@@ -74,20 +98,15 @@ function kcw_logs_api_RegisterProjectRoutes() {
 function kcw_logs_api_RegisterStaffRoutes() {
     global $kcw_logs_api_namespace;
     global $kcw_logs_auth_token_rest_format;
+    global $kcw_logs_search_rest_format;
     $token = $kcw_logs_auth_token_rest_format;
+    $search_str = $kcw_logs_search_rest_format;
     
     //Route for listing all staff
     //If the given token isnt authenticated to view staff, returns auth error
     register_rest_route( "$kcw_logs_api_namespace/v1", "/$token/staff", array(
         'methods' => 'GET',
         'callback' => 'kcw_logs_api_list_Staff',
-    ));
-
-    //Route for searching through all the staff members in the staff table (limited based on to_show)
-    //If the given token isnt authenticated to staff customers, returns auth error
-    register_rest_route( "$kcw_logs_api_namespace/v1", "/$token/staff/search/(?P<staffsearch>[a-zA-Z0-9]+)/(?P<to_show>[0-9])", array(
-        'methods' => 'GET',
-        'callback' => 'kcw_logs_',
     ));
 
     //Route for getting a staff member based on the given staff id
@@ -108,7 +127,9 @@ function kcw_logs_api_RegisterStaffRoutes() {
 function kcw_logs_api_RegisterCustomerRoutes() {
     global $kcw_logs_api_namespace;
     global $kcw_logs_auth_token_rest_format;
+    global $kcw_logs_search_rest_format;
     $token = $kcw_logs_auth_token_rest_format;
+    $search_str = $kcw_logs_search_rest_format;
     
     //Route for listing all customers
     //If the given token isnt authenticated to view customers, returns auth error
@@ -119,9 +140,9 @@ function kcw_logs_api_RegisterCustomerRoutes() {
 
     //Route for searching through all the customers in the customers table (limited based on to_show)
     //If the given token isnt authenticated to view customers, returns auth error
-    register_rest_route( "$kcw_logs_api_namespace/v1", "/$token/customers/search/(?P<customersearch>[a-zA-Z0-9]+)/(?P<to_show>[0-9])", array(
+    register_rest_route( "$kcw_logs_api_namespace/v1", "/$token/customers/search/$search_str", array(
         'methods' => 'GET',
-        'callback' => 'kcw_logs_',
+        'callback' => 'kcw_logs_api_search_Customers',
     ));
 
     //Route for getting a customer based on the given customer id
